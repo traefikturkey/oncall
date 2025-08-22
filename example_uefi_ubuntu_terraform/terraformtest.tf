a346dee7678fe2c2ea00dcc12876db1c3c52c2df
BladeWDR <github@ssb-tech.net>
2025-08-22 16:12:03 -0400

    cleaned up formatting

--- example_uefi_ubuntu_terraform/terraformtest.tf
+++ example_uefi_ubuntu_terraform/terraformtest.tf
@@ -12,47 +12,48 @@ resource "proxmox_vm_qemu" "main" {
   cpu {
     cores = var.cpu_cores
   }
+  memory = var.ram_amount
 
-    network {
-        id = 0
-        model = "virtio"
-        bridge = var.virt_switch
-        tag = var.vlantag
-        firewall = false
-    
-     }
+  network {
+    id       = 0
+    model    = "virtio"
+    bridge   = var.virt_switch
+    tag      = var.vlantag
+    firewall = false
 
-    scsihw   = "virtio-scsi-single" 
-    boot     = "order=virtio0;net0"
-    bootdisk = "virtio0"
-    
-    disks {
-        virtio {
-            virtio0 {
-                disk{
-                    storage = var.disk_datastore
-                    size    = var.disk_size
-                }
-            }
+  }
+
+  scsihw   = "virtio-scsi-single"
+  boot     = "order=virtio0;net0"
+  bootdisk = "virtio0"
+
+  disks {
+    virtio {
+      virtio0 {
+        disk {
+          storage = var.disk_datastore
+          size    = var.disk_size
         }
-        ide {
-            ide0 {
-                cloudinit {
-                    storage = var.disk_datastore
-                }
-            }
-       }
+      }
     }
-    efidisk {
-        efitype = "4m"
-        storage = var.disk_datastore
-        pre_enrolled_keys = false
-    }     
+    ide {
+      ide0 {
+        cloudinit {
+          storage = var.disk_datastore
+        }
+      }
+    }
+  }
+  efidisk {
+    efitype           = "4m"
+    storage           = var.disk_datastore
+    pre_enrolled_keys = false
+  }
 
-    # Cloud-init options
-    # Keep in mind to use the CIDR notation for the ip.
-    ipconfig0 = var.ipconfig_set
-    ciuser = var.cloud_user
-    sshkeys = file(var.pub_ssh_key_path)
+  # Cloud-init options
+  # Keep in mind to use the CIDR notation for the ip.
+  ipconfig0 = var.ipconfig_set
+  ciuser    = var.cloud_user
+  sshkeys   = file(var.pub_ssh_key_path)
 
 }
