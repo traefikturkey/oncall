beb3b6c25091e7b20ea1e63ff3ea9fcdfabc39b0
sudo-battlekafer <battlekafer@gmail.com>
2025-08-22 17:03:51 -0500

    ignoring submodules in .gitignore

--- terraform_import/import.sh
+++ terraform_import/import.sh
@@ -15,8 +15,8 @@ TFVARS_FILE="terraform.tfvars"
 vmid=$(grep -E '^vm_id\s*=' "$TFVARS_FILE" | awk -F'=' '{print $2}' | tr -d ' "')
 pvenode=$(grep -E '^node\s*=' "$TFVARS_FILE" | awk -F'=' '{print $2}' | tr -d ' "')
 
-echo -n "This will import vm $vmid from $pvenode. The import name will be 'importvm' in the $vmid.tfstate file "
-
+echo -n "This will import vm $vmid from $pvenode. The import name will be 'importvm' in the $vmid-$(date +"%Y%m%d-%H%M").tfstate file "
+echo -n ""
 terraform import proxmox_vm_qemu.importvm ${pvenode}/vm/${vmid}
 
 mv terraform.tfstate $vmid-$(date +"%Y%m%d-%H%M").tfstate
